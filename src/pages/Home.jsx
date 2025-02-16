dd1d80e37809f66b946ba7a2a06a6f8955a93941
Julie30at <167688513+Julie30at@users.noreply.github.com>
2025-02-16 13:55:31 +0100

    home page responsive et style optimisés

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from "react";
 import Navbar from "../Components/Navbar"; 
 import julieAtPhoto from '../assets/webaju_logo.webp'; 
 import '../styles/home.css';
+import About from "./About";
+import Portfolio from "./Portfolio";
+import Cv from "./Cv";
+import Contact from "./Contact";
 
 function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1024px)");
+
+    // Fonction qui met à jour `isMobile`
+    const handleResize = () => setIsMobile(mediaQuery.matches);
+
+    // Ajouter l'écouteur d'événement
+    mediaQuery.addEventListener("change", handleResize);
+
+    // Vérifier la taille initiale
+    handleResize();
+
+    // Nettoyage de l'écouteur quand le composant est démonté
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
   return (
     <div className="home_page">
       <Navbar /> 
@@ -20,6 +43,16 @@ function Home() {
           </span>
         </h2>
       </section>
+
+      {/* Affichage du contenu des autres pages si on est en mobile/tablette */}
+      {isMobile && (
+        <>
+          <About />
+          <Portfolio />
+          <Cv />
+          <Contact />
+        </>
+      )}
     </div>
   );
 }
